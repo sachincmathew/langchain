fcb6452fc38fde72143ec471a8be75d55a388807
sachincmathew <sachincmathew@gmail.com>
2025-02-02 19:47:32 +0800

    retrieving top matching results with Source

---
+++ 5_rag/2b_rag_basics_metadata.py
@@ -0,0 +1,28 @@
+import os
+
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings 
+
+# Define the embedding model
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+# Load the existing vector store with the embedding function
+index_name = "books-rag-demo"
+db = PineconeVectorStore(index_name=index_name, embedding=embeddings)
+
+# Define the user's question
+#query = "How did Juliet die?"
+query = "Who is Odysseus wife?"
+
+# Retrieve relevant documents based on the query
+retriever = db.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k": 3, "score_threshold": 0.3},
+)
+relevant_docs = retriever.invoke(query)
+
+# Display the relevant results with metadata
+print("\n--- Relevant Documents ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
+    print(f"Source: {doc.metadata['source']}\n")
